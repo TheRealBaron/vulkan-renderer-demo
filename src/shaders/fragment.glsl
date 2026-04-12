093948129509227f3df1b562e687e10742481a22
TheRealBaron <bariatamanov@mail.ru>
2026-04-12 19:35:19 +0300

    saved my models implementations

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -22,12 +22,11 @@ struct PointLight {
     float constant;
     float linear;
     float quadratic;
-
-    int use_blinn_phong;
 };
 
 
-vec3 compute_light_affect(in PointLight light, in float shine, in vec3 normal, in vec3 view_dir) {
+
+vec3 phong(in PointLight light, in float shine, in vec3 normal, in vec3 view_dir) {
     float d = length(frag_pos - light.position);
     float attitude = 1.f / (light.constant + light.linear * d + light.quadratic * d * d);
 
@@ -39,34 +38,52 @@ vec3 compute_light_affect(in PointLight light, in float shine, in vec3 normal, i
     float spec = pow(max(0, dot(reflect_dir, view_dir)), 128.f * shine);
 
     vec3 ambient = light.ambient * frag_color.rgb;
-    ambient *= attitude;
 
     vec3 diffuse = light.diffuse * diff * frag_color.rgb;
-    diffuse *= attitude;
 
     vec3 specular = light.specular * spec * frag_color.rgb;
-    specular *= attitude;
 
-    return (ambient + diffuse + specular);
+    return (ambient + diffuse + specular) * attitude;
+
 }
 
 
+vec3 blinn_phong(in PointLight light, in float shine, in vec3 normal, in vec3 view_dir) {
+    float d = length(frag_pos - light.position);
+    float attitude = 1.f / (light.constant + light.linear * d + light.quadratic * d * d);
+
+    vec3 light_dir = normalize(light.position - frag_pos);
+
+    float diff = max(0, dot(normal, light_dir));
+    
+    vec3 halfway_dir = normalize(light_dir + view_dir);
+    float spec = pow(max(0, dot(normal, halfway_dir)), 128.f * shine);
+
+    vec3 ambient = light.ambient * frag_color.rgb;
+
+    vec3 diffuse = light.diffuse * diff * frag_color.rgb;
+
+    vec3 specular = light.specular * spec * frag_color.rgb;
+
+    return (ambient + diffuse + specular) * attitude;
+}
+
+// -4.f 0.f -4.f
 const PointLight light_source = PointLight(
-    vec3(-4.f, 0.f, -4.f),
+    vec3(-3.f, 0.f, -4.f),
     vec3(0.05, 0.05, 0.05),
     vec3(0.8, 0.8, 0.8),
     vec3(0.9, 0.9, 0.9),
     1.0,
     0.09,
-    0.032,
-    0
+    0.032
 );
 
 void main() {
     //float sc = max(dot(normalize(frag_norm), normalize(light_source - frag_pos)), 0.f) + 1.f;
     //out_color = vec4(frag_color * sc, 1.0f);
     
-    vec3 final_col = compute_light_affect(
+    vec3 final_col = blinn_phong(
         light_source,
         0.5f,
         normalize(frag_norm),
